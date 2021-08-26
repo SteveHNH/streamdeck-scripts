4a4ea9dbe8d7d2ec99da98fdef49f5113bc50ff0
Stephen Adams <tsadams@gmail.com>
2021-08-26 16:50:10 -0400

    [fix] Retain brightness and temperature settings
    The brightness and temperature settings could have been changed by the
    control center app or some other source. If those have been changed we
    want to leave them alone. This fix just turns the light on and off while
    retaining those values.
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- eg-keylight.py
+++ eg-keylight.py
@@ -9,24 +9,19 @@ args = parser.parse_args()
 
 def get_state():
     response = requests.get(f"http://{args.ip}:9123/elgato/lights")
-    return response.json()["lights"][0]["on"]
+    return response.json()
 
-def set_state(value):
-    j = {"numberOfLights": 1,
-          "lights": [
-             {"on": value,
-              "brightness": 18,
-               "temperature": 288}
-             ]
-        }
-    response = requests.put(f"http://{args.ip}:9123/elgato/lights", data=json.dumps(j))
+def set_state(state, value):
+    state["lights"][0]["on"] = value
+    response = requests.put(f"http://{args.ip}:9123/elgato/lights", data=json.dumps(state))
 
 def main():
     state = get_state()
-    if state == 0:
-        set_state(1)
+    current_value = state["lights"][0]["on"]
+    if current_value == 0:
+        set_state(state, 1)
     else:
-        set_state(0)
+        set_state(state, 0)
 
 if __name__ == '__main__':
     main()
