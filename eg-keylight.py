7d8ade5ccf192e19a7df3f982ec96696ff4fa39d
Stephen Adams <tsadams@gmail.com>
2021-08-28 16:17:49 -0400

    [feat] allow brightness and temp changes
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- eg-keylight.py
+++ eg-keylight.py
@@ -4,6 +4,9 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('ip', help="the IP of the key light")
+parser.add_argument('-p', '--power', help="toggle power")
+parser.add_argument('-t', '--temp', help="the light temperature: 2900 to 7000")
+parser.add_argument('-b', '--brightness', help="the brightness level: 3 to 100")
 
 args = parser.parse_args()
 
@@ -11,17 +14,23 @@ def get_state():
     response = requests.get(f"http://{args.ip}:9123/elgato/lights")
     return response.json()
 
-def set_state(state, value):
-    state["lights"][0]["on"] = value
+def set_state(state):
     response = requests.put(f"http://{args.ip}:9123/elgato/lights", data=json.dumps(state))
 
 def main():
     state = get_state()
-    current_value = state["lights"][0]["on"]
-    if current_value == 0:
-        set_state(state, 1)
-    else:
-        set_state(state, 0)
+    if args.brightness and (args.brightness in range(3, 100)):
+        state["lights"][0]["brightness"] = args.brightness
+    if args.temp and (args.temp in range(2900, 7000)):
+        state["lights"][0]["temperature"] = args.temp
+    if args.power:
+        pwr = state["lights"][0]["on"]
+        if pwr == 0:
+            state["lights"][0]["on"] = 1
+        else:
+            state["lights"][0]["on"] = 0
+
+    set_state(state)
 
 if __name__ == '__main__':
     main()
